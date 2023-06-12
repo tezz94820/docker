266d1ce1e1c3bcb607546ff7bbeb0708549ce941
Tejas Itankar <tejasitankar94820@gmail.com>
2023-06-13 00:29:02 +0530

    initialized the node_docker project and made container for express app

---
+++ node_docker/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res)=> {
+    res.send("<h2>Welcome to the Tejas World !!!! hello again </h2>");
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port , () => console.log("Listening on port " + port))
