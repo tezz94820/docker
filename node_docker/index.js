a19b57cafb77a685d7f580a3a71340322e35da99
Tejas Itankar <tejasitankar94820@gmail.com>
2023-06-16 14:04:56 +0530

    completed communication link between node and mongo container

--- node_docker/index.js
+++ node_docker/index.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config/config');
+
+
 const app = express();
 
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+
+const connectWithRetry = () => { 
+    mongoose
+    .connect(mongoURL)
+    .then( () => console.log("Successfully connected to db"))
+    .catch( (e) => {
+        console.log(e) 
+        setTimeout(connectWithRetry, 5000);
+    })
+}
+
+connectWithRetry();
+
 app.get('/', (req,res)=> {
-    res.send("<h2>Welcome to the Tejas World !!!! hello again </h2>");
+    res.send("<h2>Welcome to the Tejas World !!!</h2>");
 })
 
 const port = process.env.PORT || 3000;
